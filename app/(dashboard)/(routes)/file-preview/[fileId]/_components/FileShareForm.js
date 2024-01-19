61840b9e6cfab3eceaaea5b9822e57a7b209da0e
steven_savarin <srssdesing@gmail.com>
2024-01-20 00:47:37 +0100

    Actualización del código del componente FileShareForm

--- app/(dashboard)/(routes)/file-preview/[fileId]/_components/FileShareForm.js
+++ app/(dashboard)/(routes)/file-preview/[fileId]/_components/FileShareForm.js
@@ -1,123 +1,116 @@
-import { Copy, CopyIcon } from 'lucide-react';
-import React, { useContext, useState } from 'react';
-import GlobalApi from './../../../../../_utils/GlobalApi';
+import { Copy, CopyIcon } from 'lucide-react'
+import React, {  useContext, useState } from 'react'
+import GlobalApi from './../../../../../_utils/GlobalApi'
 import { useUser } from '@clerk/nextjs';
 import Toast from '../../../../../_components/Toast';
 
-function FileShareForm({ file, onPasswordSave }) {
-  const [isPasswordEnable, setIsEnablePassword] = useState(false);
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [toast, setToast] = useState();
-  const { user } = useUser();
-
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-
-  const sendEmail = () => {
-    setToast({
-      status: 'Info',
-      msg: 'Sending Email...!',
-    });
-    const data = {
-      emailToSend: email,
-      userName: user?.fullName,
-      fileName: file.fileName,
-      fileSize: file.fileSize,
-      fileType: file.fileType,
-      shortUrl: `${baseUrl}f/${file.shortUrl.split('/').pop()}`,
-    };
-    GlobalApi.SendEmail(data).then((resp) => {
-      console.log(resp);
-      setToast({
-        status: 'success',
-        msg: 'Email Sent Successfully!',
-      });
-    });
-  };
-
-  const onCopyClick = () => {
-    if (file && file.shortUrl) {
-      const completeUrl = `${baseUrl}f/${file.shortUrl.split('/').pop()}`;
-      navigator.clipboard.writeText(completeUrl);
-      setToast({
-        status: 'Copied',
-        msg: 'Url Copied!',
-      });
-    } else {
-      console.error('File or file.shortUrl is undefined');
+function FileShareForm({ file,onPasswordSave }) {
+    const [isPasswordEnable,setIsEnablePassword]=useState(false);
+    const [password,setPassword]=useState('');
+    const [email,setEmail]=useState('');
+    const [toast,setToast]=useState();
+    const {user}=useUser();
+    const sendEmail=()=>{
+      
+        setToast({
+            status:'Info',
+            msg:'Sending Email...!'
+        })
+        const data={
+            emailToSend:email,
+            userName:user?.fullName,
+            fileName:file.fileName,
+            fileSize:file.fileSize,
+            fileType:file.fileType,
+            shortUrl:`http://localhost:3000/f/${file.shortUrl.split('/').pop()}`,
+        }
+        GlobalApi.SendEmail(data).then(resp=>{
+            console.log(resp);
+            setToast({
+                status:'success',
+                msg:'Email Sent Successfully!'
+            })
+        })
     }
-  };
 
-  return file && (
-    <div className='flex flex-col gap-2'>
-      <div>
-        <label className='text-[14px] text-gray-500'>Short Url</label>
-        <div className='flex gap-5 p-2 border rounded-md justify-between'>
-          <input
-            type="text"
-            value={`${baseUrl}/f/${file.shortUrl.split('/').pop()}`}
-            disabled
-            className='disabled:text-gray-500 bg-transparent outline-none w-full'
-          />
-          <Copy
-            className='text-gray-400 hover:text-gray-600 cursor-pointer'
-            onClick={() => onCopyClick()}
-          />
+    const onCopyClick = () => {
+        if (file && file.shortUrl) {
+            const completeUrl = `http://localhost:3000/f/${file.shortUrl.split('/').pop()}`;
+            navigator.clipboard.writeText(completeUrl);
+            setToast({
+                status: 'Copied',
+                msg: 'Url Copied!'
+            });
+        } else {
+            console.error("File or file.shortUrl is undefined");
+        }
+    }
+    
+    return file && (
+        <div className='flex flex-col gap-2'>
+            <div>
+                <label className='text-[14px] text-gray-500'>Short Url</label>
+                <div className='flex gap-5 p-2 border rounded-md justify-between'>
+                    {/* Construir la URL completa con el formato deseado */}
+                    <input
+                        type="text"
+                        value={`http://localhost:3000/f/${file.shortUrl.split('/').pop()}`}
+                        disabled
+                        className='disabled:text-gray-500 bg-transparent outline-none w-full'
+                    />
+                    <Copy
+                        className='text-gray-400 hover:text-gray-600 cursor-pointer'
+                        onClick={() => onCopyClick()}
+                    />
+                </div>
+          
+            </div>
+            <div className='gap-3 flex mt-5'>
+                <input type='checkbox'
+                defaultChecked={file.password!=''}
+                onChange={(e)=>setIsEnablePassword(e.target.checked)}/>
+                <label>Enable Password?</label>
+            </div>
+            
+            {isPasswordEnable?  
+            <div className='flex gap-3 items-center'>
+            <div className='border rounded-md w-full p-2'>
+                 <input type="password" 
+                 defaultValue={file.password}
+                 className='disabled:text-gray-500 bg-transparent
+                 outline-none' onChange={(e)=>setPassword(e.target.value)}/>
+            </div>
+            <button className='p-2 bg-primary text-white
+                rounded-md disabled:bg-gray-300 hover:bg-green-600' 
+                disabled={password?.length<3}
+                onClick={()=>onPasswordSave(password)}
+                >Save</button>
+            </div>:null}
+
+            <div className='border rounded-md p-3 mt-5'>
+                    <label className='text-[14px] text-gray-500'>Send File to Email</label>
+                    <div className='border rounded-md p-2'>
+                    <input type="email" 
+                    placeholder='example@gmail.com'
+                    className=' bg-transparent
+                    outline-none w-full' 
+                    onChange={(e)=>setEmail(e.target.value)}
+                    />
+                </div>
+                <button  className='p-2 disabled:bg-gray-300
+                 bg-primary text-white hover:bg-green-600
+                w-full mt-2 rounded-md'
+                disabled={email?.length<3}
+                onClick={()=>sendEmail()}
+                >Send Email</button>
+            </div>
+
+           {toast?.status? <Toast 
+           toast={toast}
+           closeToast={()=>setToast(null)}
+            />:null}
         </div>
-      </div>
-      <div className='gap-3 flex mt-5'>
-        <input
-          type='checkbox'
-          defaultChecked={file.password !== ''}
-          onChange={(e) => setIsEnablePassword(e.target.checked)}
-        />
-        <label>Enable Password?</label>
-      </div>
-
-      {isPasswordEnable ? (
-        <div className='flex gap-3 items-center'>
-          <div className='border rounded-md w-full p-2'>
-            <input
-              type="password"
-              defaultValue={file.password}
-              className='disabled:text-gray-500 bg-transparent outline-none'
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button
-            className='p-2 bg-primary text-white rounded-md disabled:bg-gray-300 hover:bg-green-600'
-            disabled={password?.length < 3}
-            onClick={() => onPasswordSave(password)}
-          >
-            Save
-          </button>
-        </div>
-      ) : null}
-
-      <div className='border rounded-md p-3 mt-5'>
-        <label className='text-[14px] text-gray-500'>Send File to Email</label>
-        <div className='border rounded-md p-2'>
-          <input
-            type="email"
-            placeholder='example@gmail.com'
-            className='bg-transparent outline-none w-full'
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <button
-          className='p-2 disabled:bg-gray-300 bg-primary text-white hover:bg-green-600 w-full mt-2 rounded-md'
-          disabled={email?.length < 3}
-          onClick={() => sendEmail()}
-        >
-          Send Email
-        </button>
-      </div>
-
-      {toast?.status ? (
-        <Toast toast={toast} closeToast={() => setToast(null)} />
-      ) : null}
-    </div>
-  );
+    )
 }
 
-export default FileShareForm;
+export default FileShareForm
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,17 +7,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC5IURFYbbFs-kCA2HWpPuoHCU7twIwkKI",
-  authDomain: "file-uploading-5dd3e.firebaseapp.com",
+  apiKey: process.env.APP_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
   projectId: "file-uploading-5dd3e",
   storageBucket: "file-uploading-5dd3e.appspot.com",
-  messagingSenderId: "684757001745",
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: "1:684757001745:web:45c04a8559d714246bf3b0",
-  measurementId: "G-KXJ773Q0G4"
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 export default app;
