f1effd018d09ef0c03d717fbe270a4073bf0a910
Savarin <137004357+Stevensavarin@users.noreply.github.com>
2024-02-06 23:59:18 +0100

    Update Hero.js

--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -4,28 +4,27 @@ import Constant from '../_utils/Constant';
 function Hero() {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = '/adsense.js';
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7761286844545012';
     script.async = true;
-    document.head.appendChild(script);
+    script.crossOrigin = 'anonymous';
+    document.body.appendChild(script);
 
     return () => {
-      document.head.removeChild(script);
+      document.body.removeChild(script);
     };
   }, []);
 
   return (
     <section className="bg-gray-50">
-      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex">
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex ">
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
             <span className='text-primary'>Upload, Save </span> 
             and easily <span className='text-primary'>Share</span> your files in one place
           </h1>
-
           <p className="mt-4 sm:text-xl/relaxed text-gray-500">
             {Constant.desc}
           </p>
-
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
               className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-green-700 focus:outline-none focus:ring active:bg-green-500 sm:w-auto"
