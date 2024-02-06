a99232bf8a1e9232fd159fbbf716353b1172f8ce
Savarin <137004357+Stevensavarin@users.noreply.github.com>
2024-02-06 23:54:03 +0100

    Update Hero.js

--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -1,47 +1,43 @@
-import React from 'react'
-import Constant from '../_utils/Constant'
+import React, { useEffect } from 'react';
+import Constant from '../_utils/Constant';
 
 function Hero() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '/adsense.js';
+    script.async = true;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <section className="bg-gray-50">
-  <div
-    className="mx-auto max-w-screen-xl px-4 py-32 
-    lg:flex "
-  >
-    <div className="mx-auto max-w-xl text-center">
-      <h1 className="text-3xl font-extrabold sm:text-5xl">
-        <span className='text-primary'>Upload, Save </span> 
-        and easily <span className='text-primary'>Share</span> your files in one place
-      </h1>
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex">
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            <span className='text-primary'>Upload, Save </span> 
+            and easily <span className='text-primary'>Share</span> your files in one place
+          </h1>
 
-      <p className="mt-4 sm:text-xl/relaxed text-gray-500">
-       {Constant.desc}
-      </p>
+          <p className="mt-4 sm:text-xl/relaxed text-gray-500">
+            {Constant.desc}
+          </p>
 
-      <div className="mt-8 flex flex-wrap justify-center gap-4">
-        <a
-          className="block w-full rounded
-           bg-primary px-12 py-3 text-sm 
-           font-medium text-white shadow
-            hover:bg-green-700 focus:outline-none focus:ring active:bg-green-500 sm:w-auto"
-          href="/upload"
-        >
-          Get Started
-        </a>
-
-        {/* <a
-          className="block w-full rounded px-12 py-3 
-          text-sm font-medium text-primary shadow
-           hover:text-green-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
-          href="/about"
-        >
-          Learn More
-        </a> */}
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-green-700 focus:outline-none focus:ring active:bg-green-500 sm:w-auto"
+              href="/upload"
+            >
+              Get Started
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
-  )
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
