e5d2cf9dea832f28975d7f2a0044978acc1a43da
Savarin <137004357+Stevensavarin@users.noreply.github.com>
2024-02-07 00:00:05 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,26 +1,13 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Header from './_components/Header';
-import Hero from './_components/Hero';
+import Image from 'next/image'
+import Header from './_components/Header'
+import Hero from './_components/Hero'
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <script src="/adsense.js" async></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (adsbygoogle = window.adsbygoogle || []).push({
-                google_ad_client: '${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}',
-                enable_page_level_ads: true
-              });
-            `,
-          }}
-        ></script>
-      </Head>
-      <Header />
-      <Hero />
+      <Header/>
+      <Hero/>
     </div>
-  );
+  )
 }
+
