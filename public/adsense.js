ae33e0405581403a5927fab4976754595cb4a0a6
Savarin <137004357+Stevensavarin@users.noreply.github.com>
2024-02-06 23:45:25 +0100

    Create adsense.js

---
+++ public/adsense.js
@@ -0,0 +1,7 @@
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = process.env.NEXT_PUBLIC_ADSENSE_SCRIPT_URL;
+    document.head.appendChild(script);
+  })();
+  
