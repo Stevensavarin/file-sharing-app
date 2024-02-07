62f17f844a202bf50989118f43a44756524b7f32
Savarin <137004357+Stevensavarin@users.noreply.github.com>
2024-02-07 01:17:10 +0100

    Update UploadForm.js

--- app/(dashboard)/(routes)/upload/_components/UploadForm.js
+++ app/(dashboard)/(routes)/upload/_components/UploadForm.js
@@ -30,7 +30,7 @@ function UploadForm({ uploadBtnClick, progress }) {
                         <p className="mb-2 text-lg md:text-2xl text-black-500 dark:text-black-400">
                             <span className="font-semibold">Click to upload </span>
                             or <strong className='text-primary'>drag</strong> and <strong className='text-primary'>drop</strong></p>
-                        <p className="text-xs text-black-500 dark:text-black-400">SVG, PNG, JPG, GIF, M4A, MP3, MP4, RAR, ZIP</p>
+                        <p className="text-xs text-black-500 dark:text-black-400">SVG, PNG, JPG, GIF, M4A, MP3, RAR, ZIP</p>
                     </div>
                     <input id="dropzone-file" type="file" className="hidden" onChange={(event) => { onFileSelect(event.target.files[0]) }} accept=".svg,.png,.jpg,.jpeg,.gif,.m4a,.mp3,.mp4,.exe,.rar,.zip" />
                 </label>
